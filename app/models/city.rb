ee1d027ea5cfe65a6d0aa9e840a320f6e12f249a
github username <kelly.hesterly@gmail.com>
2021-10-22 23:37:31 -0600

    commit tree 2929f9a350720911c3a65e5304d364b791904c8c

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,12 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(listing, date)
+    Listing.all.each do |list|
+      list.title
+      list.address
+      list.created_at
+    end
+  end
 
+end
